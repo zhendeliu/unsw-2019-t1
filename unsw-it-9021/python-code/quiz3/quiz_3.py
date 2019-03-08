296f964df342a5d42b3e37642631886e93bf1c0e
Zhende-Liu <45912039+Zhende-Liu@users.noreply.github.com>
2019-03-08 23:41:36 +1100

    update quiz3

---
+++ unsw-it-9021/python-code/quiz3/quiz_3.py
@@ -0,0 +1,73 @@
+# Written by ZD Liu and Eric Martin for COMP9021
+
+
+
+import sys
+from random import seed, randint, randrange
+
+
+try:
+    arg_for_seed, upper_bound, length =\
+            (int(x) for x in input('Enter three integers: ').split())
+except ValueError:
+    print('Incorrect input, giving up.')
+    sys.exit()
+
+
+def length_of_longest_increasing_sequence(L):
+    count_list = []
+    for i in range(len(L)):
+        j = i+1
+        if j >= len(L):
+            j = 0
+        count = 1
+        while L[i] <= L[j]:
+            count += 1
+            i += 1
+            j += 1
+            if i >= len(L):
+                i = 0
+            if j >= len(L):
+                j = 0
+        count_list.append(count)
+    return max(count_list)
+    pass
+    # REPLACE pass ABOVE WITH  YOUR CODE
+
+def max_int_jumping_in(L):
+    num_list = []
+    for i in range(len(L)):
+        num_str = str(L[i])
+        index_list = []
+        index_list.append(i)
+        index_new = L[i]
+        while index_new not in index_list:
+            index_list.append(index_new)
+            index_new = L[index_new]
+            num_str += str(index_new)
+        num_list.append(num_str)
+    res = num_list[0]
+    for s in num_list:
+        if len(res) < len(s):
+            res = s
+    return res
+    pass
+    # REPLACE pass ABOVE WITH  YOUR CODE
+
+
+seed(arg_for_seed)
+L_1 = [randint(0, upper_bound) for _ in range(length)]
+print('L_1 is:', L_1)
+print('The length of the longest increasing sequence\n'
+      '  of members of L_1, possibly wrapping around, is:',
+      length_of_longest_increasing_sequence(L_1), end = '.\n\n'
+     )
+L_2 = [randrange(length) for _ in range(length)]
+print('L_2 is:', L_2)
+print('The maximum integer built from L_2 by jumping\n'
+      '  as directed by its members, from some starting member\n'
+      '  and not using any member more than once, is:',
+      max_int_jumping_in(L_2)
+     )
+
+
